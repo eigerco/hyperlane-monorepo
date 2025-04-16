81a97379b49876148792d7eed4c606fa631069ff
Luke Lorenzini <luke.lorenzini@gmail.com>
2025-04-16 08:10:22 -0400

    Remove unused fns from rest_client

--- rust/main/chains/hyperlane-sovereign/src/provider.rs
+++ rust/main/chains/hyperlane-sovereign/src/provider.rs
@@ -67,16 +67,11 @@ impl HyperlaneProvider for SovereignProvider {
         Ok(true)
     }
 
-    // todo: what is this doing?
-    async fn get_balance(&self, address: String) -> ChainResult<U256> {
-        let token_id = "token_1nyl0e0yweragfsatygt24zmd8jrr2vqtvdfptzjhxkguz2xxx3vs0y07u7";
-        let balance = self.client.get_balance(token_id, address.as_str())?;
-        Ok(balance)
+    async fn get_balance(&self, _address: String) -> ChainResult<U256> {
+        todo!("Not yet implemented")
     }
 
     async fn get_chain_metrics(&self) -> ChainResult<Option<ChainInfo>> {
-        // let metrics = self.client.get_chain_metrics().await?;
-        // Ok(metrics)
         todo!("Not yet implemented")
     }
 }
