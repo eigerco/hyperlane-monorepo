6856a0742b56ea517b12ac19b3974a5d201cf9db
Luke Lorenzini <luke.lorenzini@gmail.com>
2025-05-15 13:41:38 -0400

    [WIP] cleanup operation_verifier

--- rust/main/chains/hyperlane-sovereign/src/application/operation_verifier.rs
+++ rust/main/chains/hyperlane-sovereign/src/application/operation_verifier.rs
@@ -1,20 +1,13 @@
-// use std::io::Cursor;
-
 use async_trait::async_trait;
 use derive_new::new;
-// use tracing::trace;
-
-use hyperlane_core::{
-    // Decode,
-    HyperlaneMessage,
-    // HyperlaneProvider, U256
-};
+use hyperlane_core::{Decode, HyperlaneMessage, U256};
 use hyperlane_operation_verifier::{
     ApplicationOperationVerifier, ApplicationOperationVerifierReport,
 };
-// use hyperlane_warp_route::TokenMessage;
+use hyperlane_warp_route::TokenMessage;
+use std::io::Cursor;
 
-// const WARP_ROUTE_MARKER: &str = "/";
+const WARP_ROUTE_MARKER: &str = "/";
 
 /// Application operation verifier for Sovereign
 #[derive(new)]
@@ -24,9 +17,32 @@ pub struct SovereignApplicationOperationVerifier {}
 impl ApplicationOperationVerifier for SovereignApplicationOperationVerifier {
     async fn verify(
         &self,
-        _app_context: &Option<String>,
-        _message: &HyperlaneMessage,
+        app_context: &Option<String>,
+        message: &HyperlaneMessage,
     ) -> Option<ApplicationOperationVerifierReport> {
+        use ApplicationOperationVerifierReport::{MalformedMessage, ZeroAmount};
+
+        let context = match app_context {
+            Some(c) => c,
+            None => return None,
+        };
+
+        if !context.contains(WARP_ROUTE_MARKER) {
+            return None;
+        }
+
+        // Starting from this point we assume that we are in a warp route context
+
+        let mut reader = Cursor::new(message.body.as_slice());
+        let token_message = match TokenMessage::read_from(&mut reader) {
+            Ok(m) => m,
+            Err(_) => return Some(MalformedMessage(message.clone())),
+        };
+
+        if token_message.amount() == U256::zero() {
+            return Some(ZeroAmount);
+        }
+
         None
     }
 }
