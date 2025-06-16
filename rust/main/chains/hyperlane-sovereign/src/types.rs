b4d3f05040c6af445677c3a58d1c6c04d351b310
zvolin <mac.zwolinski@gmail.com>
2025-06-16 13:12:33 +0200

    Add proper docs for types and replace another string with hash

--- rust/main/chains/hyperlane-sovereign/src/types.rs
+++ rust/main/chains/hyperlane-sovereign/src/types.rs
@@ -2,69 +2,95 @@ use hyperlane_core::H256;
 use serde::Deserialize;
 use serde_json::Value;
 
-/// A Sovereign Rest response payload.
+/// Event emitted during transaction execution.
 #[derive(Clone, Debug, Deserialize)]
 pub struct TxEvent {
+    /// Key of the event.
     pub key: String,
+    /// Value of the event.
     pub value: Value,
+    /// Global event's index.
     pub number: u64,
 }
 
-/// A Sovereign Rest response payload.
+/// A Sovereign transaction.
 #[derive(Clone, Debug, Deserialize)]
 pub struct Tx {
+    /// Global transaction index.
     pub number: u64,
+    /// A hash of the transaction.
     pub hash: H256,
+    /// Events emitted during execution.
     pub events: Vec<TxEvent>,
+    /// Global batch index.
     pub batch_number: u64,
+    /// A receipt of transaction execution.
     pub receipt: Receipt,
 }
 
-/// A Sovereign Rest response payload.
+/// Receipt of the transaction execution.
 #[derive(Clone, Debug, Deserialize)]
 pub struct Receipt {
+    /// The outcome of the transaction.
     pub result: String,
+    /// Transaction metadata.
     pub data: TxData,
 }
 
-/// A Sovereign Rest response payload.
+/// Transaction metadata.
 #[derive(Clone, Debug, Deserialize)]
 pub struct TxData {
+    /// Gas consumption of the transaction.
     pub gas_used: Vec<u32>,
 }
 
-/// A Sovereign Rest response payload.
+/// Batch of Sovereign transactions.
 #[derive(Clone, Debug, Deserialize)]
 pub struct Batch {
+    /// Global batch index.
     pub number: u64,
+    /// A hash of the batch.
     pub hash: H256,
+    /// Transactions in the batch.
     pub txs: Vec<Tx>,
+    /// A slot number at which batch appeared.
     pub slot_number: u64,
 }
 
-/// A Sovereign Rest response payload.
+/// A slot that may contain transaction batches.
 #[derive(Clone, Debug, Deserialize)]
 pub struct Slot {
+    /// Global slot index.
     pub number: u64,
+    /// A hash of the slot.
     pub hash: H256,
+    /// Batches in the slot, if any.
     pub batches: Vec<Batch>,
 }
 
-/// Collection of transaction statuses.
+/// Status of the transaction processing.
 #[derive(Deserialize, Debug)]
 #[serde(rename_all = "snake_case")]
 pub enum TxStatus {
+    /// Transaction wasn't found.
     Unknown,
+    /// Transaction was dropped from the pool.
     Dropped,
+    /// Transaction was successfully submitted.
     Submitted,
+    /// Transaction was included in the batch.
     Published,
+    /// Transaction was successfully processed.
     Processed,
+    /// Transaction is finalized.
     Finalized,
 }
 
-/// Transaction information.
+/// Transaction info.
 #[derive(Deserialize, Debug)]
 pub struct TxInfo {
-    pub id: String,
+    /// A hash of the transaction.
+    pub id: H256,
+    /// Transaction status.
     pub status: TxStatus,
 }
