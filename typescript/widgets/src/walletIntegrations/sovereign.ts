b30ad3b2fc3fcfc0b245cba081398a1db1a56736
ross-weir <29697678+ross-weir@users.noreply.github.com>
2025-06-27 19:05:02 +1000

    sovereign wallet integration stubs

--- typescript/widgets/src/walletIntegrations/sovereign.ts
+++ typescript/widgets/src/walletIntegrations/sovereign.ts
@@ -1,10 +1,5 @@
-import {
-  AccountInfo,
-  ActiveChainInfo,
-  ChainTransactionFns,
-  WalletDetails,
-} from './types.js';
-import {  useCallback, useMemo } from 'react';
+import { useCallback, useMemo } from 'react';
+
 import {
   ChainName,
   MultiProtocolProvider,
@@ -13,86 +8,57 @@ import {
 } from '@hyperlane-xyz/sdk';
 import { ProtocolType } from '@hyperlane-xyz/utils';
 
+import {
+  AccountInfo,
+  ActiveChainInfo,
+  ChainTransactionFns,
+  WalletDetails,
+} from './types.js';
 
 export function useSovereignAccount(
   _multiProvider: MultiProtocolProvider,
 ): AccountInfo {
-  // const { publicKey, connected, wallet } = useWallet();
-  // TODO: Add actual account connections
-  const isReady = true; //!!(publicKey && wallet && connected);
-
   return useMemo<AccountInfo>(
     () => ({
       protocol: ProtocolType.Sovereign,
       addresses: [],
-      isReady: isReady,
-    }),
-    [isReady],
-  );
-}
-
-
-export function useSovereignWalletDetails() {
-
-  return useMemo<WalletDetails>(
-    () => ({
+      isReady: true,
     }),
     [],
   );
 }
 
+export function useSovereignWalletDetails() {
+  return useMemo<WalletDetails>(() => ({}), []);
+}
+
 export function useSovereignConnectFn(): () => void {
-  // const { setVisible } = useWalletModal();
-  // return useCallback(() => setVisible(true), [setVisible]);
   return () => {};
 }
 
 export function useSovereignDisconnectFn(): () => Promise<void> {
-  // const { disconnect } = useWallet();
-  // return disconnect;
   return () => Promise.resolve();
 }
 
 export function useSovereignActiveChain(
-  multiProvider: MultiProtocolProvider,
+  _multiProvider: MultiProtocolProvider,
 ): ActiveChainInfo {
-  // const { connection } = useConnection();
-  // const connectionEndpoint = connection?.rpcEndpoint;
   return useMemo<ActiveChainInfo>(() => {
-    // try {
-    //   const hostname = new URL(connectionEndpoint).hostname;
-    //   const metadata = findChainByRpcUrl(multiProvider, hostname);
-    //   if (!metadata) return {};
-    //   return {
-    //     chainDisplayName: metadata.displayName,
-    //     chainName: metadata.name,
-    //   };
-    // } catch (error) {
-    //   logger.warn('Error finding sol active chain', error);
-    //   return {};
-    // }
     return {
       chainDisplayName: 'Sovereign Placeholder',
       chainName: 'sovereign',
-    }
-    
-  }, [multiProvider]);
+    };
+  }, []);
 }
 
 export function useSovereignTransactionFns(
-  multiProvider: MultiProtocolProvider,
+  _multiProvider: MultiProtocolProvider,
 ): ChainTransactionFns {
-  // const { sendTransaction: sendSolTransaction } = useWallet();
-
-  const onSwitchNetwork = useCallback(async (chainName: ChainName) => {
-    // logger.warn(`Sovereign SDK chain (${chainName})is not supported`);
-  }, []);
+  const onSwitchNetwork = useCallback(async (_chainName: ChainName) => {}, []);
 
   const onSendTx = useCallback(
     async ({
       tx,
-      chainName,
-      activeChainName,
     }: {
       tx: WarpTypedTransaction;
       chainName: ChainName;
@@ -100,31 +66,8 @@ export function useSovereignTransactionFns(
     }) => {
       if (tx.type !== ProviderType.Sovereign)
         throw new Error(`Unsupported tx type: ${tx.type}`);
-      // if (activeChainName && activeChainName !== chainName)
-      //   await onSwitchNetwork(chainName);
-      // const rpcUrl = multiProvider.getRpcUrl(chainName);
-      // const connection = new Connection(rpcUrl, 'confirmed');
-      // const {
-      //   context: { slot: minContextSlot },
-      //   value: { blockhash, lastValidBlockHeight },
-      // } = await connection.getLatestBlockhashAndContext();
-
-      // logger.debug(`Sending tx on chain ${chainName}`);
-      // const signature = await sendSolTransaction(tx.transaction, connection, {
-      //   minContextSlot,
-      // });
-
-      // const confirm = (): Promise<TypedTransactionReceipt> =>
-      //   connection
-      //     .confirmTransaction({ blockhash, lastValidBlockHeight, signature })
-      //     .then(() => connection.getTransaction(signature))
-      //     .then((r) => ({
-      //       type: ProviderType.SolanaWeb3,
-      //       receipt: r!,
-      //     }));
 
       return { hash: 'signature', confirm: () => Promise.reject() };
-      // return {};
     },
     [],
   );
