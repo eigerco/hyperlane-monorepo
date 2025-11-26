462dda5021df2728d87238550ed9b21a4db5a54c
ICavlek <ivan.cavlek1403@gmail.com>
2025-11-26 17:19:01 +0100

    feat: observable for alice's wallet state

--- scripts/main.ts
+++ scripts/main.ts
@@ -1,5 +1,7 @@
 import { Command } from 'commander';
-import { send } from './send.js';
+import { WalletBuilder } from '@midnight-ntwrk/wallet';
+import { NetworkId } from '@midnight-ntwrk/zswap';
+import { send, testnetWalletSeeds } from './send.js';
 import { logger } from './utils.js';
 
 const program = new Command();
@@ -15,6 +17,24 @@ program
   .description('Send tDUST tokens')
   .action(async () => {
     await send();
+  });
+
+program
+  .command('state')
+  .description('State of Alice wallet')
+  .action(async () => {
+    const wallet = await WalletBuilder.build(
+      'http://127.0.0.1:8088/api/v1/graphql',
+      'ws://127.0.0.1:8088/api/v1/graphql/ws',
+      'http://localhost:6300',
+      'http://127.0.0.1:9944',
+      testnetWalletSeeds.alice,
+      NetworkId.Undeployed
+    );
+    wallet.start();
+    wallet.state().subscribe((state) => {
+      console.log(state);
+    });
   });
 
 try {
